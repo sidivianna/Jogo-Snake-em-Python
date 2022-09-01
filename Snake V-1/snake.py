5b3c2d77f0659b83f2f27567ee9fbb944bf5139a
sidivianna <sidineirochavianna19@gmail.com>
2022-09-01 20:58:33 -0300

    cobra maça e corpo

--- Snake V-1/snake.py
+++ Snake V-1/snake.py
@@ -1,4 +1,4 @@
-# Músicas e Sons
+# Criar cobra, maça e corpo da cobra aumentar.
 
 import pygame
 from pygame.locals import *
@@ -11,15 +11,15 @@ pygame.mixer.music.set_volume(0.1)
 musica_de_fundo = pygame.mixer.music.load('sons/01.mp3')
 barulho_colisao = pygame.mixer.Sound('sons/smw_coin.wav')
 pygame.mixer.music.play(-1)
-# inicia o player de música, e configura volume.
+
 
 LARGURA = 640
 ALTURA = 480
-x = LARGURA/2 
-y = ALTURA/2
+x_cobra = LARGURA/2 
+y_cobra = ALTURA/2
 
-x_azul = randint(40, 600)
-y_azul = randint(50, 430)
+x_maca = randint(40, 600)
+y_maca = randint(50, 430)
 
 pontos = 0
 fonte = pygame.font.SysFont('arial', 40, bold=True, italic=True)
@@ -27,10 +27,16 @@ fonte = pygame.font.SysFont('arial', 40, bold=True, italic=True)
 tela = pygame.display.set_mode((LARGURA, ALTURA))
 pygame.display.set_caption('SNAKE')
 relogio = pygame.time.Clock() 
+lista_cobra = []
+
+def aumenta_cobra(lista_cobra):
+    for XeY in lista_cobra:
+        pygame.draw.rect(tela, (0,255,0),(XeY[0],XeY[1],20,20))
+        # função que desenha o corpo da cobra.
 
 while True:
     relogio.tick(30) 
-    tela.fill((0,0,0)) 
+    tela.fill((0,0,0)) # cor da tela
     mensagem = f'Pontos:{pontos}'
     texto_formatado = fonte.render(mensagem, False, (255, 255, 255))
     
@@ -40,23 +46,36 @@ while True:
             exit()
 
     if pygame.key.get_pressed()[K_RIGHT]:
-        x = x + 20
+        x_cobra = x_cobra + 20
     if pygame.key.get_pressed()[K_LEFT]:
-        x = x - 20
+        x_cobra = x_cobra - 20
     if pygame.key.get_pressed()[K_UP]:
-        y = y - 20
+        y_cobra = y_cobra - 20
     if pygame.key.get_pressed()[K_DOWN]:
-        y = y + 20
+        y_cobra = y_cobra + 20
 
-    ret_vermelho = pygame.draw.rect(tela, (255,0,0), (x,y,40,50))
-    ret_azul = pygame.draw.rect(tela, (0,0,255), (x_azul,y_azul,40,50))
+    cobra = pygame.draw.rect(tela, (0,255,0), (x_cobra,y_cobra,20,20))
+    maca = pygame.draw.rect(tela, (255,0,0), (x_maca,y_maca,20,20))
     
-    if ret_vermelho.colliderect(ret_azul):
-        x_azul = randint(40, 600)
-        y_azul = randint(50, 430)
+    if cobra.colliderect(maca):
+        x_maca = randint(40, 600)
+        y_maca = randint(50, 430)
         pontos += 1
         barulho_colisao.play()
+
+    lista_cabeca = []   
+    lista_cabeca.append(x_cobra)
+    lista_cabeca.append(y_cobra)
+    lista_cobra.append(lista_cabeca)
+    #lista para armazenar a posição da cabeça da cobra.
+
+    aumenta_cobra(lista_cobra)
+    # Chama a função do corpo da cobra crescer
  
     tela.blit(texto_formatado, (450, 40)) 
+
     pygame.display.update() 
  
+
+
+
