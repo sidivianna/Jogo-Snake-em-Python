25c643a715ce04d48d63febfcd5bbf25d5417baf
sidivianna <sidineirochavianna19@gmail.com>
2022-09-01 20:09:14 -0300

    criada a função de colisão dentro da tela

--- snake.py
+++ snake.py
@@ -1,17 +1,20 @@
 # Movendo objetos na tela
 
 import pygame
-
 from pygame.locals import *
-
 from sys import exit
+from random import randint
 
 pygame.init()
 
 LARGURA = 640
 ALTURA = 480
-x = LARGURA/2 # posição do obejto na tela
-y = 0
+x = LARGURA/2 
+y = ALTURA/2
+
+X_AZUL = randint(40, 600)
+Y_AZUL = randint(50, 430)
+# define posições randomizadas para o retângulo azul na tela.
 
 tela = pygame.display.set_mode((LARGURA, ALTURA))
 pygame.display.set_caption('SNAKE')
@@ -34,7 +37,16 @@ while True:
     if pygame.key.get_pressed()[K_DOWN]:
         y = y + 20
 
-        # definidas as funções de movimentos para os botões.
+    RET_VERMELHO = pygame.draw.rect(tela, (255,0,0), (x,y,40,50))
+    RET_AZUL = pygame.draw.rect(tela, (0,0,255), (X_AZUL,Y_AZUL,40,50))
+    # define os dois retangulos como variáveis
+    
+    if RET_VERMELHO.colliderect(RET_AZUL):
+        X_AZUL = randint(40, 600)
+        Y_AZUL = randint(50, 430)
+        # cria a função colisão e faz com que sempre que haja colisão o trinagulo azul mude de posição.
+
+        
 
     pygame.draw.rect(tela, (255,0,0), (x,y,40,50))
    
